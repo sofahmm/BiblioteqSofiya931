53b89a444917781b20fbf52dc593363d2589a4ef
Gogs <ilivic@yandex.ru>
2025-02-19 10:39:29 +0300

    Добавьте файлы проекта.

---
+++ BiblioteqSofiya931/DBConnection/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioteqSofiya931.DBConnection
+{
+    public class Connection
+    {
+        public static BiblioteqKymSofiyaEntities biblioteq = new BiblioteqKymSofiyaEntities();  
+    }
+}
